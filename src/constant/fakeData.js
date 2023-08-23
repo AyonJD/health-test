337a774a7dcf85e3cf1d009fb70db81af5063b93
AyonJD <ayonjodder177@gmail.com>
2023-08-23 21:38:46 +0600

    Data added

---
+++ src/constant/fakeData.js
@@ -0,0 +1,90 @@
+export const data =  [
+    {
+      category: "Category 1",
+      tests: [
+        {
+          test: "Test 1",
+          testPrice: 100,
+          centers: [
+            {
+              center: "Center A",
+              branches: ["Branch X", "Branch Y"]
+            },
+            {
+              center: "Center B",
+              branches: ["Branch Z"]
+            }
+          ]
+        },
+        {
+          test: "Test 2",
+          testPrice: 150,
+          centers: [
+            {
+              center: "Center C",
+              branches: ["Branch W", "Branch V"]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      category: "Category 2",
+      tests: [
+        {
+          test: "Test 2",
+          testPrice: 50,
+          centers: [
+            {
+              center: "Center A",
+              branches: ["Branch X", "Branch Y"]
+            },
+            {
+              center: "Center C",
+              branches: ["Branch X", "Branch Z"]
+            }
+          ]
+        },
+        {
+          test: "Test 3",
+          testPrice: 80,
+          centers: [
+            {
+              center: "Center C",
+              branches: ["Branch X", "Branch W", "Branch V"]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      category: "Category 3",
+      tests: [
+        {
+          test: "Test 4",
+          testPrice: 120,
+          centers: [
+            {
+              center: "Center D",
+              branches: ["Branch A", "Branch Y"]
+            },
+            {
+              center: "Center E",
+              branches: ["Branch X", "Branch W"]
+            }
+          ]
+        },
+        {
+          test: "Test 5",
+          testPrice: 30,
+          centers: [
+            {
+              center: "Center D",
+              branches: ["Branch X", "Branch V"]
+            }
+          ]
+        }
+      ]
+    },
+  ]
+
